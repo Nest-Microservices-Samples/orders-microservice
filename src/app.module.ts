ae43faf11de575ab7810f0f63975fdb0362a1f56
Nicolay <nicolaymorenoherrera@gmail.com>
2024-09-22 16:22:18 -0500

    tsconfig.json modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 
+
 @Module({
   imports: [OrdersModule, ProductsModule],
 })
